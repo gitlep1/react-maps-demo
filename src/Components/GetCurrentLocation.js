52d9efd18eaca72d784205739f6b87f832f8e558
snyles <sniles@gmail.com>
2021-07-20 20:09:59 -0400

    add error checking in geocode

--- src/Components/GetCurrentLocation.js
+++ src/Components/GetCurrentLocation.js
@@ -16,15 +16,17 @@ export default function GetCurrentLocation({setLocation}) {
             geocoder.geocode({ location: pos})
             .then((response) => {
               if (response.results[0]) {
-                const city = response.results[0].address_components.find(comp => comp.types.includes('locality')).long_name
-                const state = response.results[0].address_components.find(comp => comp.types.includes('administrative_area_level_1')).short_name
-                const location = {
-                  name: `${city}, ${state}`,
-                  coords: pos
+                const city = response.results[0]?.address_components?.find(comp => comp.types.includes('locality')).long_name
+                const state = response.results[0]?.address_components?.find(comp => comp.types.includes('administrative_area_level_1')).short_name
+                if(city && state && pos) {
+                  const location = {
+                    name: `${city}, ${state}`,
+                    coords: pos
+                  }
+                  console.log("location", location, location.coords.lat())
+                  locData.setLoc(location)
+                  setLocation(location)
                 }
-                console.log("location", location, location.coords.lat())
-                locData.setLoc(location)
-                setLocation(location)
               } else {
                 console.log("No locations found")
               }
